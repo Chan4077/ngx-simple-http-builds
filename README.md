7feae86ffa741bf607e05adbecd45e07133c4b8b
Edric Chan <edric.chan.1997@gmail.com>
2018-12-23 09:01:00 +0000

    master - c665af0 chore: remove Greenkeeper and Renovate configs

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Also ensure that your app is running at least Angular 6 before continuing.
 
 ### Installing
 
-Run the following comamnd which should install the `latest` version (`1.0.0-47cabd8-alpha.3`):
+Run the following comamnd which should install the `latest` version (`1.0.0-c665af0-alpha.3`):
 
 ```bash
 npm install ngx-simple-http --save
--- fesm5/ngx-simple-http.js
+++ fesm5/ngx-simple-http.js
@@ -6,7 +6,7 @@ import { Version, Injectable, NgModule, defineInjectable, inject } from '@angula
  * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 /** @type {?} */
-var VERSION = new Version('1.0.0-47cabd8-47cabd8');
+var VERSION = new Version('1.0.0-c665af0-c665af0');
 
 /**
  * @fileoverview added by tsickle
